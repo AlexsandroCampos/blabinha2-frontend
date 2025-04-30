343455ea01445c9bf036c34ef3a0132c563a14c1
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2025-04-30 13:38:01 -0300

    feat: add results component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GameResultsComponent } from './pages/game-results/game-results.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,10 @@ export const routes: Routes = [
             { path: '', component: HomeComponent },
         ],
         title: 'Home',
+    },
+    {
+        path: 'results',
+        component: GameResultsComponent,
+        title: 'GameResults',
     },
 ];
---
+++ src/app/pages/game-results/game-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-results',
+  imports: [],
+  templateUrl: './game-results.component.html',
+  styleUrl: './game-results.component.css'
+})
+export class GameResultsComponent {
+
+}
